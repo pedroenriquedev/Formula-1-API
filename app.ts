64229a9dfee64ed1fed0c1ec08eb75013153655e
pedroenriquedev <pedroenriquedev@gmail.com>
2023-05-31 21:17:48 -0400

    feat: script to transfer drivers data into DB

--- app.ts
+++ app.ts
@@ -10,7 +10,6 @@ app.use(express.json({
 
 const apiURL = process.env.API_URL || '/api/test';
 
-
 app.use(`${apiURL}/constructor`, routes.constructorRoute);
 app.use(`${apiURL}/circuit`, routes.circuitRoute);
 app.use(`${apiURL}/driver`, routes.driverRoute);
@@ -18,5 +17,18 @@ app.use(`${apiURL}/qualifying`, routes.qualifyingRoute);
 app.use(`${apiURL}/raceresult`, routes.raceResultRoute);
 app.use(`${apiURL}/race`, routes.raceRoute);
 app.use(`${apiURL}/sprintresult`, routes.sprintResultRoute);
+
+app.use(`${apiURL}/getdata`, async (req, res, next) => {
+	const apiurl = 'http://ergast.com/api/f1/2023/drivers.json';
+
+	const data = await fetch(apiurl);
+	const dataJson = await data.json();
+
+	console.log(dataJson.MRData.DriverTable.Drivers);
+
+	res.status(200).json({
+		message: 'data!' 
+	})
+})
 
 export default app;
--- types/ModelTypes.ts
+++ types/ModelTypes.ts
@@ -1,3 +1,68 @@
 export type Team = {
-    name: string, is_active: boolean, country: string
+    id?: string, name: string, is_active: boolean, country: string
+}
+
+export type Driver = {
+    id? : string,
+    first_name: string,
+    last_name: string,
+    birthdate: string,
+    nationality: string,
+    current_team_id: string
+}
+
+export type Circuit = {
+    id? : string,
+    name: string,
+    city: string,
+    state: string
+    country: string
+}
+
+export type Race = {
+    id?: string,
+    year?: any,
+    seasonYear?: number,
+    round: number,
+    circuit?: any,
+    circuitId?: string,
+    name: string,
+    laps: number,
+    date: string,
+    time: string,
+    quali_date: string,
+    quali_time: string,
+    sprint_date: string,
+    sprint_time: string
+}
+
+export type Qualyfying = {
+    id?: string,
+    race?: any,
+    raceId?: string,
+    driver?: any,
+    driverId?: string,
+    team?: any,
+    teamId?: string,
+    number: number,
+    position: number,
+    lap_time: string
+}
+
+export type RaceResult = {
+    id?: string,
+    race?: any,
+    raceId?: string,
+    driver?: any,
+    driverId?: string,
+    team?: any,
+    teamId: string,
+    grid_position: number,
+    dnf: boolean,
+    end_position: number,
+    points: number,
+    laps: number,
+    time: string,
+    fastest_lap: number,
+    fastest_lap_time: string
 }
